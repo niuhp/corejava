ddf483c35acdbb9aedbb7a390f064c7e5609a800
niuhp <niuhp@NIUHAIPENG.bjhuahuan.com>
2014-04-24 13:43:06 +0800

    modify package

---
+++ src/main/java/com/niuhp/corejava/algorithms/sort/HeapSortor.java
@@ -0,0 +1,11 @@
+package com.niuhp.corejava.algorithms.sort;
+
+public class HeapSortor extends Sortor {
+
+	@Override
+	public void sort(int[] array, int startIndex, int endIndex) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/main/java/com/niuhp/corejava/algorithms/sort/SelectSortor.java
@@ -0,0 +1,11 @@
+package com.niuhp.corejava.algorithms.sort;
+
+public class SelectSortor extends Sortor {
+
+	@Override
+	public void sort(int[] array, int startIndex, int endIndex) {
+	
+
+	}
+
+}
