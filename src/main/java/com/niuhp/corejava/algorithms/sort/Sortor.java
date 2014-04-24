0b3566ec4174e515c93ca93dbb443c4e628a2764
unknown <niuhp@NIUHAIPENG.bjhuahuan.com>
2014-04-24 11:21:33 +0800

    modify package

---
+++ src/main/java/com/niuhp/corejava/algorithms/sort/Sortor.java
@@ -0,0 +1,10 @@
+package com.niuhp.corejava.algorithms.sort;
+
+public abstract class Sortor {
+
+	public final void sort(int[] array){
+		sort(array, 0, array.length-1);
+	}
+	
+	public abstract void sort(int[] array,int startIndex,int endIndex);
+}
