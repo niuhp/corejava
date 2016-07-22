a7e15ca8cc95aeef3340679b288f7cb45820a446
niuhp <niu323@126.com>
2016-07-22 19:19:04 +0800

    计算线程数

---
+++ src/main/java/com/niuhp/corejava/concurrent/thradscalc/AsyncIOTask.java
@@ -0,0 +1,41 @@
+package com.niuhp.corejava.concurrent.thradscalc;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * Created by niuhaipeng on 2016/7/22.
+ */
+public class AsyncIOTask implements Runnable {
+  @Override
+  public void run() {
+    HttpURLConnection connection = null;
+    BufferedReader reader = null;
+    try {
+      String getURL = "http://baidu.com";
+      URL getUrl = new URL(getURL);
+      connection = (HttpURLConnection) getUrl.openConnection();
+      connection.connect();
+      reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+      String line;
+
+      while ((line = reader.readLine()) != null) {
+        // empty loop
+      }
+    } catch (IOException e) {
+
+    } finally {
+      if (reader != null) {
+        try {
+          reader.close();
+        } catch (Exception e) {
+        }
+      }
+      connection.disconnect();
+    }
+  }
+}
+
